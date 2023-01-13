e7d493fce332cd1d9184f29a3607545edf91ceeb
poon <poon15112@gmail.com>
2023-01-13 15:47:38 +0700

    lap14_1 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,34 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double A[] ,int N, double B[]){
+    double x = 0;
+    double y = 0;
+    double z = 1;
+    double w = 0;
+    double max = A[0];
+    double min = A[0];
+    int i = 0;
+    for(i = 0; i < N; i++){
+        x += A[i];
+    }
+    B[0] = x/N;
+    for(i = 0; i < N; i++){
+        y += pow(A[i]-B[0],2);
+    }
+    B[1] = sqrt(y/N);
+    for(i = 0; i < N; i++){
+        z *= A[i];
+    }
+    B[2] = pow(z,1/double(N));
+    for(i = 0; i < N; i++){
+        w += 1/A[i];
+    }
+    B[3] = N/w;
+    for(i = 0; i < N; i++){
+        if(A[i] > max) max = A[i];
+        if(A[i] < min) min = A[i];
+    }
+    B[4] = max;
+    B[5] = min;
+}
